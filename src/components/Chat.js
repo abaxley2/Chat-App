97f628bdcc01d05f40c6a00e88c989906a3c42fb
Austin Baxley <ausbaxley@gmail.com>
2023-05-02 14:01:15 -0400

    imported addDoc from firebase and executed preventDefaut in Chat function

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import { addDoc } from "firebase/firestore";
+
 export const Chat = () => {
-  return <div className="chat-app">Chat</div>;
+  const [newMessage, setNewMessage] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newMessage === "") return;
+  };
+
+  return (
+    <div className="chat-app">
+      <form onSubmit={handleSubmit} className="new-message-form">
+        <input
+          className="new-message-input"
+          placeholder="Type your message here..."
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <button type="submit" className="send-button">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
