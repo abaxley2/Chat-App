1ca2daaeb27a335253294a970e6c01686005a7e1
Austin Baxley <ausbaxley@gmail.com>
2023-05-04 12:34:54 -0400

    chat functioning built out and cleaned up

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -13,16 +13,21 @@ import "../components/Chat.css";
 export const Chat = (props) => {
   const { room } = props;
   const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const messagesRef = collection(db, "messages");
+
   useEffect(() => {
     const queryMessages = query(messagesRef, where("room", "==", room));
-    onSnapshot(queryMessages, (snapshot) => {
+    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
       let messages = [];
       snapshot.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
+      setMessages(messages);
     });
+    return () => unsubscribe();
   });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (newMessage === "") return;
@@ -39,6 +44,11 @@ export const Chat = (props) => {
 
   return (
     <div className="chat-app">
+      <div>
+        {messages.map((message) => (
+          <h1>{message.text}</h1>
+        ))}
+      </div>
       <form onSubmit={handleSubmit} className="new-message-form">
         <input
           className="new-message-input"
