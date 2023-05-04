6fcb45e1d0e9f2fe7d76ee8ad351c12fdd85c08a
Austin Baxley <ausbaxley@gmail.com>
2023-05-04 12:22:26 -0400

    created collection, snapshot, query, and imported them

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <div>
       {room ? (
         <div>
-          <Chat />
+          <Chat room={room} />
         </div>
       ) : (
         <div className="room">
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,40 @@
-import { useState } from "react";
-import { addDoc } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+import { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
+import "../components/Chat.css";
 
-export const Chat = () => {
+export const Chat = (props) => {
+  const { room } = props;
   const [newMessage, setNewMessage] = useState("");
-  const handleSubmit = (e) => {
+  const messagesRef = collection(db, "messages");
+  useEffect(() => {
+    const queryMessages = query(messagesRef, where("room", "==", room));
+    onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+    });
+  });
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (newMessage === "") return;
+
+    await addDoc(messagesRef, {
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      room,
+    });
+
+    setNewMessage("");
   };
 
   return (
@@ -15,6 +44,7 @@ export const Chat = () => {
           className="new-message-input"
           placeholder="Type your message here..."
           onChange={(e) => setNewMessage(e.target.value)}
+          value={newMessage}
         />
         <button type="submit" className="send-button">
           Send
